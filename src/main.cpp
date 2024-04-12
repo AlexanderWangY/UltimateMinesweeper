40a5833c43dab7d0f4d7ae3b15e6dc33beb18423
AlexanderWangY <alexander.yisu.wang@outlook.com>
2024-04-12 19:55:33 -0400

    finished socket manager class

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "json.hpp"
+#include "network/SocketManager.h"
 #include <SFML/Graphics.hpp>
 #include <SFML/Network.hpp>
 #include <SFML/Network/IpAddress.hpp>
@@ -11,66 +12,20 @@
 #include <string>
 #include <thread>
 
-using json = nlohmann::json;
-
-std::mutex mtx;
-std::condition_variable cv;
-bool messageReceived = false;
-std::string receivedMessage;
-json receivedjson;
-
-void receiveMessages(sf::TcpSocket &socket) {
-  char buffer[1024];
-  std::size_t received;
-
-  while (socket.receive(buffer, sizeof(buffer), received) == sf::Socket::Done) {
-    std::string incoming(buffer, received);
-    receivedjson = json::parse(incoming);
-
-    std::unique_lock<std::mutex> lock(mtx);
-    messageReceived = true;
-    cv.notify_one();
-  }
-}
-
 int main() {
   std::string username;
   std::cout << "What is your username?" << std::endl;
   std::cin >> username;
 
-  sf::TcpSocket socket;
-  socket.connect("127.0.0.1", 8080);
+  SocketManager sock("127.0.0.1", 8080);
 
-  std::thread receiverThread(receiveMessages, std::ref(socket));
-
-  std::thread messageProcessingThread([&]() {
-    while (true) {
-      std::unique_lock<std::mutex> lock(mtx);
-      cv.wait(lock, [&] { return messageReceived; });
-      std::cout << receivedjson["username"] << ": " << receivedjson["data"]
-                << std::endl;
-      messageReceived = false;
-    }
-  });
-
-  while (true) {
-    std::string message;
-    std::getline(std::cin, message);
-
-    if (message == "exit") {
-      break;
-    }
-
-    if (message == "") {
-      continue;
-    }
-
-    json j = {{"type", "message"}, {"username", username}, {"data", message}};
-    std::string data = j.dump();
-    socket.send(data.c_str(), data.size());
+  if (sock.connect()) {
+    std::cout << "Connected to server!\n";
+  } else {
+    std::cout << "Error connecting\n";
   }
 
-  messageProcessingThread.join();
-  receiverThread.join();
+  while (true) {
+  }
   return 0;
 }
